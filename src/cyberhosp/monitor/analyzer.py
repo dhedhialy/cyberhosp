0df3e65f618e3e244e22e3764484afefa20a2390
vreddy <vijay.reddy02@infosys.com>
2026-07-14 12:21:04 +0530

    Inherit BehaviorAnalyzer from MonitorComponent

--- src/cyberhosp/monitor/analyzer.py
+++ src/cyberhosp/monitor/analyzer.py
@@ -1,5 +1,7 @@
 """Behavioral analysis and anomaly detection engine."""
 
+from cyberhosp.monitor.base import MonitorComponent
 
-class BehaviorAnalyzer:
+
+class BehaviorAnalyzer(MonitorComponent):
     pass
