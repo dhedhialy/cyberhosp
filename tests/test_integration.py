c5795e1550a71bf31e50df6e2a9a193d5af5062b
vreddy <vijay.reddy02@infosys.com>
2026-07-14 12:17:41 +0530

    Add test for FHIRAdapter inheritance from EHRAdapter

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,8 +1,9 @@
 """Integration layer tests."""
 
+from cyberhosp.integration.base import EHRAdapter
 from cyberhosp.integration.fhir import FHIRAdapter
 
 
 class TestEHRAdapter:
-    def test_fhir_adapter_instantiable(self) -> None:
-        assert FHIRAdapter() is not None
+    def test_fhir_adapter_inherits_ehradapter(self) -> None:
+        assert isinstance(FHIRAdapter(), EHRAdapter)
